ce7d67d80b8998d90cfb462fb8fe4f4051554d4b
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-12 11:44:36 +0300

    post

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import requests
 
-
+#GET
+'''
 user_input = input("Enter Id : ")
-url = f"https://jsonplaceholder.typicode.com/todos/{user_input}"
+get_url = f"https://jsonplaceholder.typicode.com/todos/{user_input}"
 
-get_response = requests.get(url)
+get_response = requests.get(get_url)
 print(get_response.json())
+'''
+
+
+
+#POST
+to_do_item = {"userId":2 ,"title": "my to do", "completed": False}
+post_url = f"https://jsonplaceholder.typicode.com/todos"
+post_response = requests.post(post_url, to_do_item)
+
+print(post_response.json())
