a94a6fe1a220464a43e6100b426e4bfd94a4fa24
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-12 11:33:38 +0300

    get

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import requests
+
+
+user_input = input("Enter Id : ")
+url = f"https://jsonplaceholder.typicode.com/todos/{user_input}"
+
+get_response = requests.get(url)
+print(get_response.json())
